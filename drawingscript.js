a2d61b09cfde0e241ccc4685ba4a87a04f9a05a8
Glitch (hello-webpage) <none>
2021-04-18 13:09:29 +0000

    🔋😘 Checkpoint ./drawingscript.js:6829469/360

--- drawingscript.js
+++ drawingscript.js
@@ -1,27 +1,29 @@
 function draw() {
   var ctx = document.getElementById("mycanvas").getContext("2d");
+  //styles
   ctx.lineWidth = 10;
   ctx.fillStyle = "yellow";
   ctx.strokeStyle = "green";
+  //a greenline
   ctx.moveTo(0, 0);
   ctx.lineTo(200, 100);
-  ctx.fill();
-  ctx.moveTo(10, 10);
-  ctx.lineTo(300, 300);
   ctx.stroke();
+  //a yellow circle with a green outline
   ctx.beginPath();
-  
-  ctx.arc(100, 100, 90, 0, 2 * Math.PI);
   ctx.fillStyle = "yellow";
+  ctx.arc(100, 100, 90, 0, 2 * Math.PI);
+  //ctx.closePath();
   ctx.fill();
+  ctx.stroke();
+   //a red rectangle  
   ctx.fillStyle = "red";
-  ctx.rect(100, 100, 150, 80);
-  ctx.fill()
+  ctx.fillRect(100, 100, 150, 80);
+  
+  //a red semi circle
   ctx.beginPath();
-   ctx.fillStyle = "yellow";
+  //ctx.fillStyle = "yellow";
   ctx.arc(50, 75, 50, 0, 1 * Math.PI);
   ctx.stroke();
- 
   ctx.fill();
 }
 draw();
