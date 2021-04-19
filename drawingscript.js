f96be0b83a16fbf52ce7cfadd99a80e0ee3be496
Glitch (hello-webpage) <none>
2021-04-19 08:53:50 +0000

    ✋💈 Checkpoint ./drawingscript.js:6829469/443 ./style.css:6829469/10

--- drawingscript.js
+++ drawingscript.js
@@ -37,6 +37,12 @@ function draw() {
   ctx.stroke();
   ctx.fill();
   
+   ctx.beginPath();
+  ctx.fillStyle = "blue";
+  ctx.ellipse(100, 100, 50, 75, 0, 0, 2* Math.PI);
+  ctx.stroke();
+  ctx.fill();
+  
   //a greenline
 ctx.lineWidth = 5;  
   ctx.moveTo(90, 90);
@@ -77,17 +83,19 @@ function randCircle(){
   var red=Math.floor(Math.random()*255)
   var green=Math.floor(Math.random()*50)
 var blue=Math.floor(Math.random()*100)
-  ctx.fillStyle = "rgba("+red+","+green+","+blue+",0.5)";
+  ctx.fillStyle = "rgba("+red+","+green+","+blue+",0.4)";
   ctx.strokeStyle = "orange";
   ctx.beginPath();
   var r1 = Math.floor(Math.random()*200)
   var r2 = Math.floor(Math.random()*200)
   var radius= Math.floor(Math.random()*20)
-  ctx.arc(r1, r2, radius, 0, 2 * Math.PI);
+  var radius2= Math.floor(Math.random()*20)
+  //ctx.arc(r1, r2, radius, 0, 2 * Math.PI);
+  ctx.ellipse(r1, r2, radius, radius2, 1.2, 0, 2* Math.PI);
   ctx.fill();
   //ctx.stroke();
 }
-setInterval(randCircle,100);
+setInterval(randCircle,50);
 
 
 
