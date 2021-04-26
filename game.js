91df51833398809c56b18ede3375ade9f9bc4926
Glitch (hello-webpage) <none>
2021-04-26 16:14:21 +0000

    🐵👨‍❤️‍💋‍👨 Checkpoint ./game.js:6829469/113

--- game.js
+++ game.js
@@ -1,12 +1,18 @@
+var oldx, oldy;
 function getCursorPosition(canvas, event) {
   var rect = canvas.getBoundingClientRect();
   var x = event.clientX - rect.left;
   var y = event.clientY - rect.top;
+  ctx.moveTo(oldx,oldy);
+  oldx=x
+  oldy=y
+  ctx.lineTo(x,y)
+  ctx.stroke()
   console.log("mouse at x: " + x + " y: " + y);
   
   ctx.beginPath();
   ctx.fillStyle = "red";
-  ctx.arc(x, y, 10, 0, 2 * Math.PI);
+  ctx.arc(x, y, 4, 0, 2 * Math.PI);
   ctx.fill();
  // ctx.beginPath()
  // ctx.lineTo(x,y)
