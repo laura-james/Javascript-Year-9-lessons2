ae424999be99145484a873c8ccef3b600807c633
Glitch (hello-webpage) <none>
2021-04-27 11:09:09 +0000

    📟🍮 Checkpoint ./game.js:6829469/3502

--- game.js
+++ game.js
@@ -1,24 +1,23 @@
-var oldx, oldy;
+/*var oldx, oldy;
 function getCursorPosition(canvas, event) {
   var rect = canvas.getBoundingClientRect();
   var x = event.clientX - rect.left;
   var y = event.clientY - rect.top;
-  ctx.moveTo(oldx,oldy);
-  oldx=x
-  oldy=y
-  ctx.lineTo(x,y)
-  ctx.stroke()
+  ctx.moveTo(oldx, oldy);
+  oldx = x;
+  oldy = y;
+  ctx.lineTo(x, y);
+  ctx.stroke();
   console.log("mouse at x: " + x + " y: " + y);
-  
+
   ctx.beginPath();
   ctx.fillStyle = "red";
   ctx.arc(x, y, 4, 0, 2 * Math.PI);
   ctx.fill();
- // ctx.beginPath()
- // ctx.lineTo(x,y)
+  // ctx.beginPath()
+  // ctx.lineTo(x,y)
 }
-var canvas = document.getElementById("mycanvas");
-var ctx = canvas.getContext("2d");
+
 canvas.addEventListener("wheel", function(e) {
   //changeRadius(canvas, e);
 });
@@ -26,41 +25,47 @@ canvas.addEventListener("wheel", function(e) {
 canvas.addEventListener("mousedown", function(e) {
   getCursorPosition(canvas, e);
 });
-
-function keyPress(code){
-  console.log(code);
-  if(code == 37){
-    console.log("LEFT")
-    playerX=playerX-50;
+*/
+var canvas = document.getElementById("mycanvas");
+var ctx = canvas.getContext("2d");
+function keyPress(code) {
+  //console.log(code);
+  if (code == 37) {
+    //console.log("LEFT")
+    playerX = playerX - 50;
   }
-   if(code == 39){
-    console.log("RIGHT")
-     playerX=playerX+50;
+  if (code == 39) {
+    //console.log("RIGHT")
+    playerX = playerX + 50;
   }
-   if(code == 38){
-    console.log("UP")
+  if (code == 38) {
+    //console.log("UP");
+    playerY = playerY - 50;
   }
-   if(code == 40){
-    console.log("DOWN")
+  if (code == 40) {
+    //console.log("DOWN");
+    playerY = playerY + 50;
   }
 }
 addEventListener("keydown", e => keyPress(e.keyCode));
-function drawGrid(){
-  for(var i = 0;i<10;i++){
-    ctx.moveTo(0,i*50);
-    ctx.lineTo(500,i*50);
+function drawGrid() {
+  ctx.fillStyle = "white";
+  ctx.fillRect(0, 0, 500, 500);
+  for (var i = 0; i < 10; i++) {
+    ctx.moveTo(0, i * 50);
+    ctx.lineTo(500, i * 50);
     ctx.stroke();
-    ctx.moveTo(i*50,0);
-    ctx.lineTo(i*50,500);
+    ctx.moveTo(i * 50, 0);
+    ctx.lineTo(i * 50, 500);
     ctx.stroke();
   }
 }
-function drawPlayer(x,y){
-  ctx.fillStyle="red";
-  ctx.fillRect(x,y,50,50);
+function drawPlayer() {
+  drawGrid();
+  ctx.fillStyle = "red";
+  ctx.fillRect(playerX, playerY, 50, 50);
 }
 var playerX = 0;
 var playerY = 0;
 
-drawGrid()
-drawPlayer(playerX,playerY);
+setInterval(drawPlayer, 1);
